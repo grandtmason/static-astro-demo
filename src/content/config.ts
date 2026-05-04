ff7caf3358278f418a1250908053791eff748b44
grandtmason <grandtmason@gmail.com>
2026-05-05 00:31:46 +0200

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,1 +1,37 @@
+import { defineCollection, z } from 'astro:content';
 
+const species = defineCollection({
+  type: 'content',
+  schema: z.object({
+    name: z.string().optional(),
+    scientific_name: z.string().optional(),
+    family: z.string().optional(),
+    native_region: z.string().optional(),
+    image: z.string().optional(),
+    images: z.array(z.object({
+      src: z.string(),
+      credit: z.string().default(''),
+    })).optional(),
+    description: z.string().optional(),
+    introduction: z.string().optional(),
+    conservation_status: z.string().optional(),
+    naming_origins: z.string().optional(),
+    common_names: z.record(z.string()).optional(),
+    medicinal_properties: z.object({
+      active_compounds: z.array(z.string()).optional(),
+      traditional_uses: z.array(z.string()).optional(),
+      clinically_validated: z.array(z.string()).optional(),
+    }).optional(),
+    cultivation: z.object({
+      climate: z.string().optional(),
+      regions: z.string().optional(),
+      commercial: z.string().optional(),
+    }).optional(),
+    indigenous_knowledge: z.string().optional(),
+    quadrants: z.object({
+      wellness: z.string().optional(),
+      innovation: z.string().optional(),
+      ik: z.string().optional(),
+      culture: z.string().optional(),
+      agronomy: z.string().optional(),
+      legislati
