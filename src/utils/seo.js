bc9b8ecbfc03241d7f8f1191ccb463bc2e6c833d
grandtmason <grandtmason@gmail.com>
2026-07-02 06:01:17 +0200

    Update seo.js

--- src/utils/seo.js
+++ src/utils/seo.js
@@ -1,1 +1,9 @@
-
+export function generatePlantSchema(data) {
+  return {
+    "@context": "https://schema.org",
+    "@type": "Plant",
+    "name": data.name,
+    "scientificName": data.scientific_name,
+    "family": data.family
+  };
+}
