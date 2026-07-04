d9096c1ed4822c10aa61fd35c88ab57cd80ffd8f
grandtmason <grandtmason@gmail.com>
2026-07-04 10:16:00 +0200

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,12 @@
-
+// src/utils/constants.ts
+export const TABS = [
+  { id: 'datasheet', label: 'Data Sheet' },
+  { id: 'wellness', label: 'Health' },
+  { id: 'innovation', label: 'Innovation' },
+  { id: 'ik', label: 'IK' },
+  { id: 'people', label: 'People' },
+  { id: 'agronomy', label: 'Agronomy' },
+  { id: 'legislation', label: 'Legislation' },
+  { id: 'projects', label: 'Projects' },
+  { id: 'intelligence', label: 'Intelligence' },
+];
