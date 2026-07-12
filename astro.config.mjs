584abbd46f0d1f365487b7475d80e245ee92c256
grandtmason <grandtmason@gmail.com>
2026-07-12 12:51:08 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://southafricanbotanical.org.za',
+  base: '/', // Try '/' first. If it fails, change this to '/static-astro-demo/'
   output: 'static',
   integrations: [sitemap()],
   build: {
