4822e2e1a41257618e711f56a6394d538d107d64
grandtmason <grandtmason@gmail.com>
2026-07-12 12:33:28 +0200

    Add sitemap integration and update build format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://southafricanbotanical.org.za',
   output: 'static',
+  integrations: [sitemap()],
   build: {
+    format: 'directory',
     inlineStylesheets: 'always'
   }
 });
