3fad3f76270e717503b1962b47460fda7fe1e8ac
grandtmason <grandtmason@gmail.com>
2026-07-12 10:43:37 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node'; // Add this adapter
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://southafricanbotanical.org.za',
-  output: 'server', // Changed from 'static' to 'server'
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'static', // Back to static for host compatibility
+  adapter: node({ mode: 'standalone' }),
   integrations: [sitemap()],
   build: {
-    inlineStylesheets: 'always',
+    inlineStylesheets: 'always'
   }
 });
