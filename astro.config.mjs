4e45f1ccf4bfbb7e83cc0adf92204d94f53c8ca3
grandtmason <grandtmason@gmail.com>
2026-07-12 15:27:38 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://southafricanbotanical.org.za',
-  output: 'static',
+  output: 'hybrid',
+  adapter: cloudflare(),
   integrations: [sitemap()],
   build: {
     format: 'file',
