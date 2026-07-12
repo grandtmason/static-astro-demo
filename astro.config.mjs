d51239ada10b3ad367a428e90b64304c20ee86ff
grandtmason <grandtmason@gmail.com>
2026-07-12 14:44:04 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,8 @@ export default defineConfig({
   output: 'static',
   integrations: [sitemap()],
   build: {
-    // Changing format to 'file' ensures each route becomes a specific .html file
-    // This often resolves issues where deployment crawlers 'prune' directories
     format: 'file',
     inlineStylesheets: 'always'
   },
-  // Ensure the deployment platform treats the root as the source of truth
   base: '/'
 });
