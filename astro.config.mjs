8e7d29b5a13e34e7eac5f4e5a709861c09129ad2
grandtmason <grandtmason@gmail.com>
2026-07-12 11:46:43 +0200

    Simplify astro.config by removing node adapter
    Removed node adapter and build configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://southafricanbotanical.org.za',
-  output: 'static',
-  adapter: node({ mode: 'standalone' }),
-  integrations: [sitemap()],
-  build: {
-    inlineStylesheets: 'always'
-  }
+  output: 'static', 
+  integrations: [sitemap()]
 });
