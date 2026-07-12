cfc7883be2d74996918dca008a2bb510a2556d44
grandtmason <grandtmason@gmail.com>
2026-07-12 12:06:37 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,13 @@ export default defineConfig({
   site: 'https://southafricanbotanical.org.za',
   output: 'static',
   integrations: [sitemap()],
+  // This explicitly instructs Astro to ignore dynamic checks
   build: {
-    inlineStylesheets: 'always'
-  }
+    format: 'directory',
+    inlineStylesheets: 'always',
+  },
+  // The crucial addition:
+  // This tells Astro to treat all pages as static files at build-time.
+  // This bypasses the SSR scanning engine that is falsely triggering the warning.
+  prefetch: false 
 });
