ba5fa3ae3ec6dfc353d0b5570ab0f4965a71cb9b
grandtmason <grandtmason@gmail.com>
2026-07-17 22:30:20 +0200

    Update seo.js

--- src/utils/seo.js
+++ src/utils/seo.js
@@ -1,9 +1,38 @@
 export function generatePlantSchema(data) {
-  return {
+  if (!data) return null;
+
+  const schema = {
     "@context": "https://schema.org",
     "@type": "Plant",
     "name": data.name,
+    "alternateName": data.scientific_name,
     "scientificName": data.scientific_name,
-    "family": data.family
+    "family": data.family,
   };
+
+  if (data.native_region) {
+    schema.nativeTo = data.native_region;
+  }
+
+  if (data.description) {
+    schema.description = data.description;
+  }
+
+  if (data.introduction) {
+    schema.disambiguatingDescription = data.introduction;
+  }
+
+  if (data.conservation_status) {
+    schema.additionalProperty = {
+      "@type": "PropertyValue",
+      "name": "Conservation Status",
+      "value": data.conservation_status,
+    };
+  }
+
+  if (data.image) {
+    schema.image = `https://southafricanbotanical.org.za${data.image}`;
+  }
+
+  return schema;
 }
